a9ebc288de8747acaf40144cf86aeba1d0dd4e0f
Johann <jteske0104@gmail.com>
2024-05-29 22:41:22 +0200

    fix: center header

--- nextjs-auth/components/auth/header.tsx
+++ nextjs-auth/components/auth/header.tsx
@@ -14,8 +14,10 @@ interface HeaderProps {
 export const Header = ({ label }: HeaderProps) => {
   return (
     <div className="w-full flex flex-col gap-y-4 item-center justify-center">
-      <h1 className={cn("text-3xl font-semibold", font.className)}>🔐 Auth</h1>
-      <p className="text-muted-foreground text-sm">{label}</p>
+      <h1 className={cn("text-3xl font-semibold text-center", font.className)}>
+        🔐 Auth
+      </h1>
+      <p className="text-muted-foreground text-sm text-center">{label}</p>
     </div>
   );
 };
