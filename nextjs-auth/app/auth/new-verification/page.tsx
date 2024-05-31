d2913e1f93057c3fda4d7a84028fc3dc1bbae30d
Johann <jteske0104@gmail.com>
2024-05-31 20:40:33 +0200

    chore: add email verification redirection site and logic

---
+++ nextjs-auth/app/auth/new-verification/page.tsx
@@ -0,0 +1,7 @@
+import { NewVerficiationForm } from "@/components/auth/new-verification-form";
+
+const NewVerificationPage = () => {
+  return <NewVerficiationForm />;
+};
+
+export default NewVerificationPage;
