904be06d2cd92b73174067e7eed50d86acee5a63
Johann <jteske0104@gmail.com>
2024-06-05 03:01:03 +0200

    chore: add example admin role gate for client and server actions

---
+++ nextjs-auth/hooks/user-current-role.ts
@@ -0,0 +1,8 @@
+// hooks can be used in client components
+import { useSession } from "next-auth/react";
+
+export const useCurrentRole = () => {
+  const session = useSession();
+
+  return session.data?.user.role;
+};
--- nextjs-auth/lib/auth.ts
+++ nextjs-auth/lib/auth.ts
@@ -1,7 +1,14 @@
+// can be used in server components, server actions and api routes
 import { auth } from "@/auth";
 
 export const currentUser = async () => {
   const session = await auth();
 
   return session?.user;
+};
+
+export const currentRole = async () => {
+  const session = await auth();
+
+  return session?.user.role;
 };
